8176f5fabeb5e9e36c67375b13afd0e114f45f95
JoaquinFerman <joacoferman@gmail.com>
2025-11-19 22:59:29 -0300

    mi fix a ver si anda

--- server/src/auth/admin.guard.ts
+++ server/src/auth/admin.guard.ts
@@ -8,15 +8,12 @@ export class AdminGuard implements CanActivate {
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     const request = context.switchToHttp().getRequest();
-    console.log('request' + request)
-    const user = request.user;
-    console.log('user' + request)
 
-    if (!user) {
+    if (!request.perfil) {
       throw new ForbiddenException('Usuario no autenticado');
     }
 
-    if (user.perfil !== 'administrador') {
+    if (request.perfil !== 'administrador') {
       throw new ForbiddenException('Acceso denegado. Se requiere perfil de administrador');
     }
 
